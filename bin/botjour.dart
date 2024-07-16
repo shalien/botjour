678ef1ccd59b9a4b484d16bb0f34eb5f79fd7733
Ophélien DUPARC <ophelien.duparc@projetretro.io>
2024-07-16 14:05:41 +0200

    Revert "v2.0.0"
    This reverts commit d34e403349d7561008a16e7e4d28ab6cfe80f6f9.

--- bin/botjour.dart
+++ bin/botjour.dart
@@ -1,7 +1,8 @@
+
+
 import 'dart:io';
 
-import 'package:bel_adn/bel_adn.dart';
-import 'package:botjour/botjour.dart';
+import 'package:botjour/src/bonjour_madame_client.dart';
 import 'package:nyxx/nyxx.dart';
 import 'package:rss_dart/dart_rss.dart';
 import 'package:sha_env/sha_env.dart';
@@ -9,22 +10,21 @@ import 'package:sha_env/sha_env.dart';
 Future<void> main() async {
   await load();
 
-  if (fromEnvironmentString('DISCORD_GUILD_ID').isEmpty) {
+  if(fromEnvironmentString('DISCORD_GUILD_ID').isEmpty) {
     stderr.writeln('DISCORD_GUILD_ID is not set');
     exit(1);
   }
 
   String guildId = fromEnvironmentString('DISCORD_GUILD_ID');
 
-  if (fromEnvironmentString('BEAU_GOSSE_DISCORD_TOKEN').isEmpty) {
+  if(fromEnvironmentString('BEAU_GOSSE_DISCORD_TOKEN').isEmpty) {
     stderr.writeln('BEAU_GOSSE_DISCORD_TOKEN is not set');
     exit(1);
   }
 
   String token = fromEnvironmentString('BEAU_GOSSE_DISCORD_TOKEN');
 
-  final NyxxGateway client =
-      await Nyxx.connectGateway(token, GatewayIntents.allUnprivileged);
+  final NyxxGateway client = await Nyxx.connectGateway(token, GatewayIntents.allUnprivileged);
 
   client.onReady.listen((ReadyEvent e) async {
     print('Bot is ready');
@@ -33,7 +33,7 @@ Future<void> main() async {
     final RssItem? item = await BonjourMadameClient().getLastUsableItem();
 
     if (item == null) {
-      stderr.writeln('No item found');
+     stderr.writeln('No item found');
       exit(1);
     }
 
@@ -41,46 +41,16 @@ Future<void> main() async {
     final String luiFirstImage = item.content!.images.first;
     final String? link = item.link;
 
-    if (link == null) {
+    if(link == null) {
       stderr.writeln('No link found');
       exit(1);
-    }
-
-    Uri url = Uri.parse(link);
-
-    if (await MagnifiqueCoupleUtils().bonjourMadamePostExists(url)) {
-      print('Post already exists');
-      exit(0);
-    }
-
-    Source? source = await MagnifiqueCoupleUtils().saveBonjourMadamePost(url);
-
-    if (source == null) {
-      print('Post already exists');
-      exit(0);
-    }
-
-    Destination? destination =
-        await MagnifiqueCoupleUtils().saveBonjourMadameDestination(url);
-
-    if (destination == null) {
-      print('Destination already exists');
-      exit(0);
-    }
-
-    Media? media = await MagnifiqueCoupleUtils()
-        .saveBonjourMadameMedia(url, source, destination);
-
-    if (media == null) {
-      print('Media already exists');
-      exit(0);
     }
 
     GuildTextChannel channel = (await client.channels
         .fetch(Snowflake(1154137527842775180))) as GuildTextChannel;
 
     EmbedImageBuilder? eib =
-        EmbedImageBuilder(url: Uri.tryParse(luiFirstImage.split('?').first)!);
+    EmbedImageBuilder(url: Uri.tryParse(luiFirstImage.split('?').first)!);
 
     await channel.sendMessage(
       MessageBuilder(
@@ -103,4 +73,6 @@ Future<void> main() async {
 
     exit(0);
   });
+
+
 }
--- lib/botjour.dart
+++ lib/botjour.dart
@@ -1,4 +1,3 @@
 library;
 
 export 'src/bonjour_madame_client.dart';
-export 'src/magnifique_couple_utils.dart';
